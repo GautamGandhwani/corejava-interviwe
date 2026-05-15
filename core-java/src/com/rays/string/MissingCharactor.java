c7d884d0bb6a4e7cdc150411d97ce1d0d5001621
GautamGandhwani <gandhwanigautam@gmail.com>
2026-05-15 12:09:45 +0530

    15-05-2026

---
+++ core-java/src/com/rays/string/MissingCharactor.java
@@ -0,0 +1,25 @@
+package com.rays.string;
+
+public class MissingCharactor {
+
+	public static void main(String[] args) {
+
+		String name = "gautam gandhwani";
+
+		int count = 0;
+
+		for (char ch = 'a'; ch <= 'z'; ch++) {
+
+			for (int i = 0; i < name.length(); i++) {
+
+				if (ch == name.charAt(i)) {
+					count++;
+				}
+			}
+			if (count == 0) {
+				System.out.print(ch + ",");
+			}
+			count = 0;
+		}
+	}
+}
